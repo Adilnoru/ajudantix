fe12176c6e0f45dc01eccf935ab6b740706ede3f
Jules <jules.pijey@gmail.com>
2020-11-13 22:58:23 +0000

    Correcção função + teste verifica valor valido

--- calculadora_subsidio_ferias/calculadora_subsidio_ferias.py
+++ calculadora_subsidio_ferias/calculadora_subsidio_ferias.py
@@ -10,12 +10,12 @@ def subsidio_ferias_diario(salario_hora, horas_semanais):
 
 def subsidio_ferias(salario, horas_semanais, dias_ferias):
     verifica_valor_valido(dias_ferias, "Dias de férias", 22)
-	salario_hora = salario_hora(salario, horas_semanais)
-	subsidio_ferias_diario = subsidio_ferias_diario(salario_hora, horas_semanais)
-    return subsidio_ferias_diario * dias_ferias
+    salario_hora_val = salario_hora(salario, horas_semanais)
+    subsidio_ferias_diario_val = subsidio_ferias_diario(salario_hora_val, horas_semanais)
+    return subsidio_ferias_diario_val * dias_ferias
 
 def verifica_valor_valido(valor, nome, limite_superior=None):
-    if type(valor) is not (int or float):
+    if type(valor) is (not int) or (not float):
         raise TypeError("O valor {} tem que ser um número!".format(nome))
     if valor <= 0:
         raise ValueError("O valor {} tem que ser maior que zero!".format(nome))
